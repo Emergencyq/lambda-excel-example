daf2ebbf10ca8759b345fbca38d6efb46a48b042
김현영 <beingtyson@gmail.com>
2019-11-24 03:26:47 +0900

    no message

--- handler.js
+++ handler.js
@@ -1,28 +1,22 @@
 'use strict';
 var xl = require('excel4node');
 module.exports.server = async (event, context, callback) => {
-  let fileName = 'Excel.xlsx'
-  var wb = new xl.Workbook();
-  var ws = wb.addWorksheet('Sheet 1');
-  ws.cell(1, 1).string('Hello~!');
-  // wb.write(fileName);
+  const fileName = 'Excel.xlsx'
 
-  let buffer = await wb.writeToBuffer();
-  console.log(buffer)
-  console.log(buffer.length)
-  let response = {
+  const wb = new xl.Workbook();
+  const ws = wb.addWorksheet('Sheet 1');
+  ws.cell(1, 1).string('Hello~!');
+
+  const bufferData = await wb.writeToBuffer();
+
+  return {
     statusCode: 200,
     headers: {
-      'Content-Length': buffer.length,
+      'Content-Length': bufferData.length,
       'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
       'Content-Disposition': `attachment; filename="${encodeURIComponent(fileName)}"; filename*=utf-8''${encodeURIComponent(fileName)};`,
     },
-    body: buffer.toString('base64'),
+    body: bufferData.toString('base64'),
     isBase64Encoded: true
   };
-  return response;
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify(buffer),
-  // };
 };
